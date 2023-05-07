fd87714d6a46ee722845df098f343cd9b81ee917
Marcos Bueno <marcos.filho@facens.br>
2023-05-07 14:37:49 -0300

    added statusbar customization

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from 'react-native';
+import { Text, View, StatusBar } from 'react-native';
 import {
   useFonts,
   Roboto_400Regular,
@@ -8,5 +8,21 @@ import {
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
-  return <View>{fontsLoaded ? <Text>Hello World!</Text> : <View />}</View>;
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#202024',
+      }}
+    >
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Text>Hello World!</Text> : <View />}
+    </View>
+  );
 }
