ca984c3c4b7eae2e5d1f431e2d607b3b693457cc
Marcos Bueno <marcos.filho@facens.br>
2023-05-08 20:32:41 -0300

    userPhoto component

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,11 +1,22 @@
 import { HStack, Heading, Text, VStack } from 'native-base';
+import { UserPhoto } from './UserPhoto';
 
 export function HomeHeader() {
   return (
-    <HStack>
+    <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
+      <UserPhoto
+        source={{ uri: 'https://github.com/marcos-bueno.png' }}
+        alt="Imagem do usuário"
+        size={16}
+        mr={4}
+      />
       <VStack>
-        <Text color="gray.100">Olá</Text>
-        <Heading color="gray.100">Marcos</Heading>
+        <Text color="gray.100" fontSize="md">
+          Olá
+        </Text>
+        <Heading color="gray.100" fontSize="md">
+          Marcos
+        </Heading>
       </VStack>
     </HStack>
   );
