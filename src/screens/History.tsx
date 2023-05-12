cbe307cc9ed958bc571a2f090e9db645043fa122
Marcos Bueno <marcos.filho@facens.br>
2023-05-12 15:32:06 -0300

    empty list message

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,9 +1,8 @@
-import { Heading, VStack } from 'native-base';
-import { SectionList } from 'native-base';
+import { Heading, VStack, SectionList, Text } from 'native-base';
+import { useState } from 'react';
 
 import { ScreenHeader } from '@components/ScreenHeader';
 import { HistoryCard } from '@components/HistoryCard';
-import { useState } from 'react';
 
 export function History() {
   const [exercises, setExercises] = useState([
@@ -25,6 +24,15 @@ export function History() {
           </Heading>
         )}
         px={8}
+        contentContainerStyle={
+          exercises.length === 0 && { flex: 1, justifyContent: 'center' }
+        }
+        ListEmptyComponent={() => (
+          <Text color="gray.100" textAlign="center">
+            Não há exercícios registrados ainda.{'\n'} Vamos fazer exercícios
+            hoje?
+          </Text>
+        )}
       />
     </VStack>
   );
