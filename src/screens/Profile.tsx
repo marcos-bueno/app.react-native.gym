c2ef749e683081ba3c0f88c19268e6d36ab2fb6d
Marcos Bueno <marcos.filho@facens.br>
2023-05-12 16:03:54 -0300

    skeleton effect

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,18 +1,33 @@
-import { VStack, ScrollView, Center } from 'native-base';
+import { useState } from 'react';
+import { VStack, ScrollView, Center, Skeleton } from 'native-base';
 import { ScreenHeader } from '@components/ScreenHeader';
 import { UserPhoto } from '@components/UserPhoto';
 
+const PHOTO_SIZE = 33;
+
 export function Profile() {
+  const [photoIsLoading, setPhotoIsLoading] = useState(true);
+
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
       <ScrollView>
         <Center mt={6} px={10}>
-          <UserPhoto
-            source={{ uri: 'https://github.com/marcos-bueno.png' }}
-            alt="Foto do usuário"
-            size={33}
-          />
+          {photoIsLoading ? (
+            <Skeleton
+              w={PHOTO_SIZE}
+              h={PHOTO_SIZE}
+              rounded="full"
+              startColor="gray.500"
+              endColor="gray.400"
+            />
+          ) : (
+            <UserPhoto
+              source={{ uri: 'https://github.com/marcos-bueno.png' }}
+              alt="Foto do usuário"
+              size={PHOTO_SIZE}
+            />
+          )}
         </Center>
       </ScrollView>
     </VStack>
