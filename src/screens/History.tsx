8acfc6c90c1fc27b7fd7c6a3824205727a851113
Marcos Bueno <marcos.filho@facens.br>
2023-05-12 15:09:43 -0300

    added historyCard component

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,10 +1,15 @@
-import { ScreenHeader } from '@components/ScreenHeader';
 import { VStack } from 'native-base';
+
+import { ScreenHeader } from '@components/ScreenHeader';
+import { HistoryCard } from '@components/HistoryCard';
 
 export function History() {
   return (
     <VStack flex={1}>
       <ScreenHeader title="Histórico" />
+
+      <HistoryCard />
+      <HistoryCard />
     </VStack>
   );
 }
