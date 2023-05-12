5992e0c205f029151529373f8e39af56ab813303
Marcos Bueno <marcos.filho@facens.br>
2023-05-12 18:09:04 -0300

    added go back button

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -1,9 +1,24 @@
-import { Center, Text } from 'native-base';
+import { VStack, Icon } from 'native-base';
+import { TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { Feather } from '@expo/vector-icons';
+
+import { AppNavigatorRoutesProps } from '@routes/app.routes';
 
 export function Exercise() {
+  const navigation = useNavigation<AppNavigatorRoutesProps>();
+
+  function handleGoBack() {
+    navigation.goBack();
+  }
+
   return (
-    <Center flex={1}>
-      <Text>Exercise</Text>
-    </Center>
+    <VStack flex={1}>
+      <VStack px={8} bg="gray.600" pt={12}>
+        <TouchableOpacity onPress={handleGoBack}>
+          <Icon as={Feather} name="arrow-left" color="green.500" size={6} />
+        </TouchableOpacity>
+      </VStack>
+    </VStack>
   );
 }
