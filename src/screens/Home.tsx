4e545340ea4c8a673a2b73b4619cc9e47ab93341
Marcos Bueno <marcos.filho@facens.br>
2023-05-12 18:03:58 -0300

    added exercise screen navigation

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,5 +1,8 @@
 import { useState } from 'react';
 import { FlatList, HStack, Heading, VStack, Text } from 'native-base';
+import { useNavigation } from '@react-navigation/native';
+
+import { AppNavigatorRoutesProps } from '@routes/app.routes';
 
 import { HomeHeader } from '@components/HomeHeader';
 import { Group } from '@components/Group';
@@ -19,6 +22,12 @@ export function Home() {
     'Levantamento terras',
   ]);
   const [groupSelected, setGroupSelected] = useState('Costas');
+
+  const navigation = useNavigation<AppNavigatorRoutesProps>();
+
+  function handleOpenExerciseDetails() {
+    navigation.navigate('exercise');
+  }
 
   return (
     <VStack flex={1}>
@@ -59,7 +68,9 @@ export function Home() {
         <FlatList
           data={exercises}
           keyExtractor={(item) => item}
-          renderItem={({ item }) => <ExerciseCard />}
+          renderItem={({ item }) => (
+            <ExerciseCard onPress={handleOpenExerciseDetails} />
+          )}
           showsVerticalScrollIndicator={false}
           _contentContainerStyle={{
             paddingBottom: 20,
