ab9a042361f01403e17e7e1dc2e4bea1bf61e0b5
Marcos Bueno <marcos.filho@facens.br>
2023-05-13 18:16:50 -0300

    state inputs

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -5,12 +5,22 @@ import LogoSvg from '@assets/logo.svg';
 import BackgroundImg from '@assets/background.png';
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
+import { useState } from 'react';
 
 export function SignUp() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirm, setPasswordConfirm] = useState('');
+
   const navigation = useNavigation();
 
   function handleGoBack() {
     navigation.goBack();
+  }
+
+  function handleSignUp() {
+    console.log(name);
   }
 
   return (
@@ -35,14 +45,24 @@ export function SignUp() {
           <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
             Crie sua conta
           </Heading>
-          <Input placeholder="Nome" />
+          <Input placeholder="Nome" onChangeText={setName} />
           <Input
             placeholder="E-mail"
             keyboardType="email-address"
             autoCapitalize="none"
+            onChangeText={setEmail}
           />
-          <Input placeholder="Senha" secureTextEntry />
-          <Button title="Criar e acessar" />
+          <Input
+            placeholder="Senha"
+            secureTextEntry
+            onChangeText={setPassword}
+          />
+          <Input
+            placeholder="Confirme a senha"
+            secureTextEntry
+            onChangeText={setPasswordConfirm}
+          />
+          <Button title="Criar e acessar" onPress={handleSignUp} />
         </Center>
         <Button
           title="Voltar para o login"
