2da0883b97ec8427621c202b6fc7ab08ec4c196e
Marcos Bueno <marcos.filho@facens.br>
2023-05-17 16:01:22 -0300

    redirecting logged in user

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,8 +2,8 @@ import { useTheme, Box } from 'native-base';
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 
 import { useAuth } from '@hooks/useAuth';
-import { AuthRoutes } from './auth.routes';
 import { AppRoutes } from './app.routes';
+import { AuthRoutes } from './auth.routes';
 
 export function Routes() {
   const { colors } = useTheme();
@@ -17,7 +17,7 @@ export function Routes() {
   return (
     <Box flex={1} bg="gray.700">
       <NavigationContainer theme={theme}>
-        <AuthRoutes />
+        {user.id ? <AppRoutes /> : <AuthRoutes />}
       </NavigationContainer>
     </Box>
   );
