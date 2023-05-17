6c9fbae93ad62e9d93cd8152511303e2773803ec
Marcos Bueno <marcos.filho@facens.br>
2023-05-17 14:49:18 -0300

    signIn refactoring

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,17 +1,35 @@
+import { Controller, useForm } from 'react-hook-form';
 import { useNavigation } from '@react-navigation/native';
 import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base';
 
 import { AuthNavigatorRoutesProps } from '@routes/auth.routes';
+
 import LogoSvg from '@assets/logo.svg';
 import BackgroundImg from '@assets/background.png';
+
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
+
+type FormData = {
+  email: string;
+  password: string;
+};
 
 export function SignIn() {
   const navigation = useNavigation<AuthNavigatorRoutesProps>();
 
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+
   function handleNewAccount() {
     navigation.navigate('signUp');
+  }
+
+  function handleSignIn({ email, password }: FormData) {
+    console.log(email, password);
   }
 
   return (
@@ -27,30 +45,59 @@ export function SignIn() {
           resizeMode="contain"
           position="absolute"
         />
+
         <Center my={24}>
           <LogoSvg />
+
           <Text color="gray.100" fontSize="sm">
-            Treine sua mente e o seu corpo
+            Treine sua mente e o seu corpo.
           </Text>
         </Center>
+
         <Center>
           <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
-            Acesse sua conta
+            Acesse a conta
           </Heading>
-          <Input
-            placeholder="E-mail"
-            keyboardType="email-address"
-            autoCapitalize="none"
+
+          <Controller
+            control={control}
+            name="email"
+            rules={{ required: 'Informe o e-mail' }}
+            render={({ field: { onChange } }) => (
+              <Input
+                placeholder="E-mail"
+                keyboardType="email-address"
+                autoCapitalize="none"
+                onChangeText={onChange}
+                errorMessage={errors.email?.message}
+              />
+            )}
           />
-          <Input placeholder="Senha" secureTextEntry />
-          <Button title="Acessar" />
+
+          <Controller
+            control={control}
+            name="password"
+            rules={{ required: 'Informe a senha' }}
+            render={({ field: { onChange } }) => (
+              <Input
+                placeholder="Senha"
+                secureTextEntry
+                onChangeText={onChange}
+                errorMessage={errors.password?.message}
+              />
+            )}
+          />
+
+          <Button title="Acessar" onPress={handleSubmit(handleSignIn)} />
         </Center>
+
         <Center mt={24}>
           <Text color="gray.100" fontSize="sm" mb={3} fontFamily="body">
             Ainda não tem acesso?
           </Text>
+
           <Button
-            title="Criar conta"
+            title="Criar Conta"
             variant="outline"
             onPress={handleNewAccount}
           />
