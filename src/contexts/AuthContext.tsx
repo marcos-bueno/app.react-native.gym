05c4948ed1726d682328cc323c7939eb9d74a5ab
Marcos Bueno <marcos.filho@facens.br>
2023-05-17 16:23:33 -0300

    added loading while loading user

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -6,6 +6,7 @@ import { storageUserGet, storageUserSave } from '@storage/storageUser';
 export type AuthContextDataProps = {
   user: UserDTO;
   signIn: (email: string, password: string) => Promise<void>;
+  isLoadingUserStorageData: boolean;
 };
 
 type AuthContextProviderProps = {
@@ -18,6 +19,7 @@ export const AuthContext = createContext<AuthContextDataProps>(
 
 export function AuthContextProvider({ children }: AuthContextProviderProps) {
   const [user, setUser] = useState<UserDTO>({} as UserDTO);
+  const [isLoadingUserStorageData, setLoadingUserStorageData] = useState(true);
 
   async function signIn(email: string, password: string) {
     try {
@@ -33,10 +35,17 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
   }
 
   async function loadUserData() {
-    const userLogged = await storageUserGet();
+    try {
+      const userLogged = await storageUserGet();
 
-    if (userLogged) {
-      setUser(userLogged);
+      if (userLogged) {
+        setUser(userLogged);
+        setLoadingUserStorageData(false);
+      }
+    } catch (error) {
+      throw error;
+    } finally {
+      setLoadingUserStorageData(false);
     }
   }
 
@@ -45,7 +54,7 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user, signIn }}>
+    <AuthContext.Provider value={{ user, signIn, isLoadingUserStorageData }}>
       {children}
     </AuthContext.Provider>
   );
