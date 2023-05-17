6ca1585b412a33568f71a367de63ce96204e549a
Marcos Bueno <marcos.filho@facens.br>
2023-05-17 15:29:29 -0300

    handleSignIn exception handling

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,6 +1,14 @@
 import { Controller, useForm } from 'react-hook-form';
 import { useNavigation } from '@react-navigation/native';
-import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base';
+import {
+  VStack,
+  Image,
+  Text,
+  Center,
+  Heading,
+  ScrollView,
+  useToast,
+} from 'native-base';
 
 import { AuthNavigatorRoutesProps } from '@routes/auth.routes';
 
@@ -11,6 +19,7 @@ import { useAuth } from '@hooks/useAuth';
 
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
+import { AppError } from '@utils/AppError';
 
 type FormData = {
   email: string;
@@ -18,6 +27,7 @@ type FormData = {
 };
 
 export function SignIn() {
+  const toast = useToast();
   const { signIn } = useAuth();
 
   const navigation = useNavigation<AuthNavigatorRoutesProps>();
@@ -33,7 +43,20 @@ export function SignIn() {
   }
 
   async function handleSignIn({ email, password }: FormData) {
-    await signIn(email, password);
+    try {
+      await signIn(email, password);
+    } catch (error) {
+      const isAppError = error instanceof AppError;
+      const title = isAppError
+        ? error.message
+        : 'Não foi possível entrar. Tente novamente mais tarde.';
+
+      toast.show({
+        title,
+        placement: 'top',
+        bgColor: 'red.500',
+      });
+    }
   }
 
   return (
